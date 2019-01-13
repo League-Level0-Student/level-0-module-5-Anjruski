89ccd5784d5da73d0b32f48338ff09bd37e5dfdb
Anjruski <league@WorthyMini-5.attlocal.net>
2019-01-13 15:58:15 -0800

    :)
    :D

--- src/algorithms/PrimeorNotiboi.java
+++ src/algorithms/PrimeorNotiboi.java
@@ -5,12 +5,16 @@ import javax.swing.JOptionPane;
 public class PrimeorNotiboi {
 public static void main(String[] args) {
 	
-	
-	String number = JOptionPane.showInputDialog("Choose a number that you think is prime.");
-		int numberint = Integer.parseInt(number);	
-		for (int i = 0; i < ; i++) {
-			
+	String xs = JOptionPane.showInputDialog("Enter a number that you think is prime");
+	int x = Integer.parseInt(xs);
+	for (int i=2; i < x; i++) {
+		if (x % i == 0) {
+			JOptionPane.showMessageDialog(null, "That number is not prime. (remember, prime numbers are only able to be divided by 1 and itself");
+			System.exit(0);
 		}
+		
+		
+	}
 	
 	
 	
