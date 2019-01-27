9b1fcde057de943f60949721a538faba75d7a9dd
Anjruski <league@WorthyMini-5.attlocal.net>
2019-01-27 15:56:14 -0800

    ;D
    yay :D

--- src/algorithms/PrimeorNotiboi.java
+++ src/algorithms/PrimeorNotiboi.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 	int x = Integer.parseInt(xs);
 	for (int i=2; i < x; i++) {
 		if (x % i == 0) {
-			JOptionPane.showMessageDialog(null, "That number is not prime. (remember, prime numbers are only able to be divided by 1 and itself");
+			JOptionPane.showMessageDialog(null, "That number is not prime. (remember, prime numbers are only able to be divided by 1 and itself)");
 			System.exit(0);
 		}
 		
---
+++ src/nested_loops/ForaGauntletboi.java
@@ -0,0 +1,47 @@
+package nested_loops;
+
+public class ForaGauntletboi {
+public static void main(String[] args) {
+
+	for (int i = 0; i < 100; i++) {
+		System.out.println(i);
+	}
+	for (int j = 100; j < 0; j++) {
+		System.out.println(j);
+	}
+	for (int a = 2; a < 100; a++) {
+		if(a % 2 == 0) {
+	System.out.println(a);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
